32526d32cd753a5e5dedb0c8023cac2acab863d5
den5509 <den5509@mail.ru>
2017-05-09 17:05:45 +0300

    latest

---
+++ init.sh
@@ -0,0 +1,13 @@
+
+cd /work
+
+make clean && make install
+
+[ "$?" != "0" ] && echo "Error 1" && exit 1
+
+echo Start
+bin/lab4
+
+[ "$?" = "0" ] \
+    && echo "Profit!" && exit 0\
+|| echo "Error 2" && exit 1
